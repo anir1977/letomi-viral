136d5c2a21a3b11e7ddbe6fa34912b7e2adf6e54
anir1977 <yuba1977@gmail.com>
2026-02-09 08:49:22 +0000

    Move AdSense script to head and add ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Fraunces, Manrope } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import SiteHeader from "./components/SiteHeader";
 import ScrollToTop from "./components/ScrollToTop";
@@ -47,12 +46,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${manrope.variable} ${fraunces.variable} antialiased`}>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9750203778031302"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
         <ReadingProgress />
         <SiteHeader />
         {children}
